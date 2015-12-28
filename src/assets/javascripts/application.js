6dfbcd8628ac65cd4f18851f2385d5a6f10864e9
Mike Ball <mikedball@gmail.com>
2015-12-27 21:26:12 -0500

    now geo-locating & fetching data

--- src/assets/javascripts/application.js
+++ src/assets/javascripts/application.js
@@ -1,1 +1,19 @@
-alert('blam!')
+(function() {
+  function getLocation(callback) {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(callback);
+    }
+  }
+
+  getLocation(function(pos) {
+    var url = 'https://septascheduler.herokuapp.com/point?route=34&lat=' + pos.coords.latitude + '&lng=' + pos.coords.longitude;
+
+    fetch(url).then(function(response) {
+      return response.json();
+    }).then(function(data) {
+      console.log(data);
+    }).catch(function(err) {
+      console.log(console.log(err));
+    });
+  });
+})();
