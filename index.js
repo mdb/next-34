d39c8c42bce194c15f3e696da90a7060a764ec0b
Mike Ball <mikedball@gmail.com>
2016-08-13 10:10:58 -0400

    remove to-be-un-used styles

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory } from 'react-router';
 import Home from './app/screens/Home';
 import User from './app/screens/User';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/styles.css';
 
 ReactDOM.render(
