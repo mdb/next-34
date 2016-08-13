ea58347c6e52f9af67d539a0d5300fca9a73e6e8
Mike Ball <mikedball@gmail.com>
2016-08-13 11:01:11 -0400

    basic rendering of day

--- app/screens/User/components/Profile.js
+++ app/screens/User/components/Profile.js
@@ -1,20 +1,25 @@
 import React, {Component, PropTypes} from 'react';
-import Tooltip from 'react-tooltip';
-import {getUserData} from '../../../utils/github-api'
-import ProfileStat from './ProfileStat'
+import {getSchedule} from '../../../utils/septa-api'
 
 export default class Profile extends Component {
   constructor() {
     super()
-    this.state = {user: {}, orgs: []}
+    this.state = {schedule: {
+      inbound: [{
+        day: ''
+      }]
+    }}
   }
 
   getUser() {
     const {username} = this.props
-    getUserData(username)
-      .then(({user, orgs}) => {
-        this.setState({user, orgs});
-      });
+    getSchedule({
+      lat: '39.947714',
+      lng: '-75.223356',
+      route: '34'
+    }).then((schedule) => {
+      this.setState({schedule});
+    });
   }
 
   componentWillMount() {
@@ -22,33 +27,11 @@ export default class Profile extends Component {
   }
 
   render() {
-    const {user, orgs} = this.state;
+    const {schedule} = this.state;
     return (
       <div>
         <section className="user">
-          <img
-            src={user.avatar_url}
-            alt="User Avatar"
-          />
-          <h2>{user.name}</h2>
-          <h5>{user.login}</h5>
-        </section>
-        <section className="stats">
-          <ProfileStat value={user.followers} label="followers" />
-          <ProfileStat value={user.public_repos} label="repositories" />
-          <ProfileStat value={user.following} label="following" />
-        </section>
-        <section className="orgs">
-          <h4>Organizations</h4>
-          {orgs.map(org => (
-            <img
-              key={org.id}
-              src={org.avatar_url}
-              alt="Organization Avatar"
-              data-tip={org.login}
-            />
-          ))}
-          <Tooltip effect="solid" />
+          <h2>{schedule.inbound[0].day}</h2>
         </section>
       </div>
     );
--- app/screens/User/index.js
+++ app/screens/User/index.js
@@ -1,27 +1,16 @@
 import React, {Component, PropTypes} from 'react';
 import Profile from './components/Profile'
-import RepoFilter from './components/RepoFilter'
-import RepoList from './components/RepoList'
 
 export default class User extends Component {
   constructor() {
     super()
-    this.state = {filter: ''}
-  }
-
-  handleFilterUpdate = (filter) => {
-    this.setState({filter})
   }
 
   render() {
     const {username} = this.props.params
-    const {filter} = this.state
     return (
       <div className="container">
         <Profile username={username} />
-        <h3>Repositories</h3>
-        <RepoFilter onUpdate={this.handleFilterUpdate} />
-        <RepoList filter={filter} username={username} />
       </div>
     );
   }
